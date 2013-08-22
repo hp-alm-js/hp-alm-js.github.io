c6e42896332639d9ec0e53940317602bf4d90c05
Vanuan <vanuan@gmail.com>
2013-08-22 18:37:12 +0300

    Add attachments support

--- hp-alm.js
+++ hp-alm.js
@@ -15,11 +15,17 @@ ALM.config = function(apiUrl, domain, project) {
 ALM.ajax = function ajax(path, onSuccess, onError) {
     $.ajax(API_URL + path, {
         success: function (response) {
+            var response;
             try {
-                onSuccess($.xml2json(response));
+                response = $.xml2json(response);
             }
             catch(err) {
-                onError(err)
+                if (onError) {
+                    onError(err)
+                }
+            }
+            if (response) { 
+                onSuccess(response);
             }
         },
         error: onError,
@@ -48,26 +54,43 @@ ALM.logout = function logout(cb) {
     ALM.ajax("authentication-point/logout", cb, function err(){});
 }
 
+function convertFields(entities) {
+    if (!(entities instanceof Array)) {
+        entities = [entities];
+    }
+    return entities.map(function (entity) {
+        var entityObj = entity.Fields.Field.reduce(function(prev, current) {
+            prev[current.Name] = current.Value;
+            return prev;
+        }, {});
+        return entityObj;
+    });
+}
+
+ALM.getDefectAttachments = function getDefectAttachments(defectId, cb, errCb) {
+    var path = "rest/domains/" + DOMAIN +
+               "/projects/" + PROJECT +
+               "/defects/" + defectId + "/attachments";
+    ALM.ajax(path, function onSuccess(attachmentsJSON) {
+        var attachments = convertFields(attachmentsJSON.Entity);
+        var buildAttachmentUrl = function(attachment) {
+            attachment.url = API_URL + path + "/"  + attachment.name;
+        }
+        attachments.map(buildAttachmentUrl);
+        cb(attachments);
+    });
+}
+
 ALM.getDefects = function getDefects(cb, errCb, query) {
-    var fields = ["id","name","description","dev-comments","severity"];
+    var fields = ["id","name","description","dev-comments","severity","attachment"];
     var fieldsParam = "fields="+ fields.join(",") + "&";
     var queryParam = "query={" + query + "}&";
     var path = "rest/domains/" + DOMAIN +
                "/projects/" + PROJECT +
                "/defects?" + queryParam + fieldsParam;
     ALM.ajax(path, function onSuccess(defectsJSON) {
-        var myDefects = defectsJSON.Entity;
         var defectsCount = defectsJSON.TotalResults;
-        if (!(myDefects instanceof Array)) {
-            myDefects = [myDefects];
-        }
-        var defects = myDefects.map(function (myDefect) {
-            var defect = myDefect.Fields.Field.reduce(function(prev, current) {
-                prev[current.Name] = current.Value;
-                return prev;
-            }, {});
-            return defect;
-        });
+        var defects = convertFields(defectsJSON.Entity);
         cb(defects, defectsCount);
     }, errCb);
 }

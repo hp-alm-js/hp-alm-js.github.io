16cb3d70664d908477740459e9ce8d561a694b5d
John Yani <vanuan@gmail.com>
2013-08-25 16:50:11 +0300

    Bring back ssl server for testing purposes.

---
+++ simulation/app.js
@@ -0,0 +1,17 @@
+var connect = require('connect'),
+    https = require('https'),
+    fs = require('fs'),
+    app = connect().use(connect.static(__dirname))
+                 .use(function(req, res, next){
+        var body = 'my page';
+        res.setHeader('Content-Length', body.length);
+        res.end(body);
+    });
+var options = {
+    key:    fs.readFileSync('ssl/server.key'),
+    cert:   fs.readFileSync('ssl/server.crt')
+};
+https.createServer(
+ options, app
+).listen(443);
+
